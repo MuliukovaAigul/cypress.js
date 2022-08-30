4384b3b642bae2daea7e2606a80dc25c57c48662
Айгуль <109337396+MuliukovaAigul@users.noreply.github.com>
2022-08-30 22:25:15 +0300

    Update staya2.js

--- cypress/integration/1-My-tests/staya2.js
+++ cypress/integration/1-My-tests/staya2.js
@@ -3,8 +3,8 @@ describe('Тестирование staya', function () {
     it('Проверка авторизации', function () {
         cy.visit('https://staya.dog/');
         cy.get('#stickyHeader > section.header-bottom.transition-header > div > div > a').click();
-        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(1)').type('mulyukova.89@mail.ru');
-        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(2)').type('mulyukovaAIGUL38242');
+        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(1)').type('ввод почты');
+        cy.get('#__layout > div > main > div > div > div > section > div > form > input:nth-child(2)').type('ввод пароля');
         cy.get('#__layout > div > main > div > div > div > section > div > form > button').click();
         cy.contains('Мои заказы');
     })
